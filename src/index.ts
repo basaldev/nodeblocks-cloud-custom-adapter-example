e2f9d118528ec54ad58556dc7beeba0d00896bcc
Shinsuke Marty Yamada <marty@basal.dev>
2024-10-29 11:41:29 +0900

    ✨ feature:　add custom routes with db operations

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,94 @@
 import * as sdk from "@basaldev/blocks-backend-sdk";
-import { defaultAdapter } from "@basaldev/blocks-user-service";
-import { isAdult } from "./utils";
+import { createNodeblocksUserApp } from "@basaldev/blocks-user-service";
+
+type StartServiceArgs = Parameters<ReturnType<typeof createNodeblocksUserApp>['startService']>;
+type ServiceOpts = StartServiceArgs[0];
 
 /**
- * A hook function called after the adapter is created
- * This hook can be used to customize the adapter instance
- * 
- * @param {defaultAdapter.UserDefaultAdapter} adapter Default adapter instance
- * @returns {defaultAdapter.UserDefaultAdapter} Updated adapter instance
+ * Define the RoleEntity class
  */
-export function adapterCreated(adapter: defaultAdapter.UserDefaultAdapter): defaultAdapter.UserDefaultAdapter {
-  const ageOfMajority = process.env.ADAPTER_CUSTOM_AGE_OF_MAJORITY
-    ? parseInt(process.env.ADAPTER_CUSTOM_AGE_OF_MAJORITY)
-    : 18;
+class RoleEntity extends sdk.mongo.BaseMongoEntity {
+  constructor(
+    public name: string,
+    public permissions: string[],
+  ) {
+    super();
+  }
+}
+
+/**
+ * A hook function called before the service is started
+ * This hook can be used to customize the options for starting the service
+ * 
+ * @param {ServiceOpts} currentOptions Service options
+ * @returns {StartServiceArgs} Updated service start args
+ */
+export async function beforeStartService(currentOptions: ServiceOpts): Promise<StartServiceArgs> {
+  const adapter = await currentOptions.adapter
+  const db = (adapter as any).dependencies.db as sdk.mongo.Db;
+  const roleRepository = new sdk.mongo.MongoRepository<RoleEntity>(db, 'roles');
 
   /**
-   * Customize validators for createUser handler
-   * https://docs.nodeblocks.dev/docs/how-tos/customization/customizing-adapters#customizing-handlers-and-validators-for-an-existing-endpoint
+   * Add new api endpoints here
+   * https://docs.nodeblocks.dev/docs/how-tos/customization/customizing-adapters#adding-new-api-endpoints
    */
-  const updatedAdapter = sdk.adapter.setValidator(adapter, 'createUser', 'ageValidator', async (logger, context) => {
-    logger.info('ageValidator');
-    const birthday = context.body.customFields?.birthday;
-    if (!birthday || !isAdult(birthday, ageOfMajority)) {
-      logger.warn('ageValidator: birthday is undefined or not adult.');
-      throw new sdk.NBError({
-        code: 'ageValidator',
-        httpCode: 400,
-        message: `your age is under ${ageOfMajority}, according to your birthday: ${birthday}`,
-      });
-    }
-    return 200;
-  });
-
-  return updatedAdapter;
+  const updatedOptions = {
+    ...currentOptions,
+    customRoutes: [
+      {
+        method: 'post' as const,
+        path: '/roles',
+        validators: [],
+        handler: async (logger: sdk.Logger, context: sdk.adapter.AdapterHandlerContext) => {
+          const name = context.body.name.toString();
+          const permissions = context.body.permissions;
+          const res = await roleRepository.create(new RoleEntity(name, permissions));
+          return {
+            data: res,
+            status: 201
+          };
+        },
+      },
+      {
+        method: 'get' as const,
+        path: '/roles',
+        validators: [],
+        handler: async (logger: sdk.Logger, context: sdk.adapter.AdapterHandlerContext) => {
+          const res = await roleRepository.findWithPagination({ filter: {}, sortParams: [] });
+          return {
+            data: res,
+            status: 200
+          };
+        },
+      },
+      {
+        method: 'patch' as const,
+        path: '/roles/:roleId',
+        validators: [],
+        handler: async (logger: sdk.Logger, context: sdk.adapter.AdapterHandlerContext) => {
+          const roleId = context.params?.roleId.toString() ?? '';
+          const permissions = context.body.permissions;
+          const res = await roleRepository.update(roleId, { permissions });
+          return {
+            data: res,
+            status: 200
+          };
+        },
+      },
+      {
+        method: 'delete' as const,
+        path: '/roles/:roleId',
+        validators: [],
+        handler: async (logger: sdk.Logger, context: sdk.adapter.AdapterHandlerContext) => {
+          const roleId = context.params?.roleId.toString() ?? '';
+          const res = await roleRepository.delete(roleId);
+          return {
+            data: { success: res },
+            status: 204
+          };
+        },
+      },
+    ]
+  };
+  return [updatedOptions];
 }
